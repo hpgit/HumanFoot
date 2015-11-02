d09c57f5bca398b617097f9ddbe3ff02022e54b3
trif <trif@trif-iMac.local>
2015-11-02 19:06:12 +0900

    hehe

--- PyCommon/externalLibs/VirtualPhysics2010/vpLib/tmp/Win32/gccRelease/rmatrix3.d
+++
@@ -1,3 +0,0 @@
-rmatrix3.o: rmatrix3.cpp ../usr/include/VP/rmatrix3.h \
-  ../usr/include/VP/LieGroup.h ../usr/include/VP/LieGroup.inl \
-  ../usr/include/VP/rmatrix3friend.inl ../usr/include/VP/rmatrix3.inl
