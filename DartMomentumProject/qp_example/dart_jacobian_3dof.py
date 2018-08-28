af19aeed06dede918dbc8ea90eb970a58c03b0d2
Hwangpil Park <garethvlf@gmail.com>
2018-08-28 11:41:08 +0900

    custom jacobian computation

--- DartMomentumProject/qp_example/dart_jacobian_3dof.py
+++ DartMomentumProject/qp_example/dart_jacobian_3dof.py
@@ -86,7 +86,127 @@ def compute_jacobian(skel, idx_or_name, eff_global_pos):
         else:
             raise NotImplementedError("only ball joint")
 
+    return J
 
 
+def compute_J_dJdq(skel):
+    """
 
+    :param skel:
+    :type skel: pydart.Skeleton
+    :return:
+    """
+    body_num = skel.num_bodynodes()
+    joint_num = skel.num_joints()
+    active_joint_index = list()
+    for i in range(joint_num):
+        if len(skel.joint(i).dofs) > 0:
+            active_joint_index.append(i)
 
+    ndofs = skel.num_dofs()
+
+    J = np.zeros((6*body_num, ndofs))
+    dJdq = np.zeros(6*body_num)
+
+    ##############################################
+    # pre-processing
+    ##############################################
+    # get joint frames
+    joint_frames = list()  # type: list[np.ndarray]
+    for joint_idx in active_joint_index:
+        joint = skel.joint(joint_idx)  # type: pydart.Joint
+        joint_frames.append(joint.get_world_frame_after_transform())
+
+    # get ancestors
+    ancestors = list()  # type: list[list[int]]
+    for body_idx in range(body_num):
+        parent_joint = skel.body(body_idx).parent_joint
+        body_ancestors = list()
+        while parent_joint is not None:
+            body_ancestors.append(parent_joint.id)
+            parent_joint = parent_joint.parent_bodynode.parent_joint if parent_joint.parent_bodynode is not None else None
+
+        ancestors.append(body_ancestors)
+
+    ##############################################
+    # root joint
+    ##############################################
+    # jacobian
+    _Jw = joint_frames[0][:3, :3]
+    for body_idx in range(body_num):
+        eff_pos = skel.body(body_idx).to_world()
+        offset = eff_pos - joint_frames[0][3, :3]
+        _Jv = -np.dot(mm.getCrossMatrixForm(offset), _Jw)
+
+        J[6*body_idx+0:6*body_idx+3, 0:3] = np.eye(3)
+        if ANGULAR_FIRST:
+            J[6*body_idx+3:6*body_idx+6, 3:6] = _Jv
+            J[6*body_idx+0:6*body_idx+3, 3:6] = _Jw
+        else:
+            J[6*body_idx+0:6*body_idx+3, 3:6] = _Jv
+            J[6*body_idx+3:6*body_idx+6, 3:6] = _Jw
+
+    # jacobian derivative
+    joint_global_pos = joint_frames[0][3, :3]
+    #TODO: check global velocity
+    joint_global_velocity = skel.body(0).world_linear_velocity(skel.joint(0).transform_from_child_body_node()[3, :3])
+    joint_global_ang_vel = skel.body(0).world_angular_velocity()
+
+    _dJdqw = np.zeros(3)
+    for body_idx in range(body_num):
+        eff_pos = skel.body(body_idx).to_world()
+        offset = eff_pos - joint_global_pos
+        eff_vel = skel.body(body_idx).world_linear_velocity()
+        offset_velocity = eff_vel - joint_global_velocity
+        _dJdqv = -mm.cross(offset, _dJdqw) - mm.cross(offset_velocity, joint_global_ang_vel)
+
+        if ANGULAR_FIRST:
+            dJdq[6*body_idx+3:6*body_idx+6] += _dJdqv
+            dJdq[6*body_idx+0:6*body_idx+3] += _dJdqw
+        else:
+            dJdq[6*body_idx+0:6*body_idx+3] += _dJdqv
+            dJdq[6*body_idx+3:6*body_idx+6] += _dJdqw
+
+    ##############################################
+    # internal joint
+    ##############################################
+    for joint_idx in active_joint_index[1:]:
+        joint = skel.joint(joint_idx)  # type: pydart.Joint
+        parent_joint_index = joint.parent_bodynode.parent_joint.id
+        dof_start_index = joint.dofs[0].index_in_skeleton()
+        joint_global_pos = joint_frames[joint_idx][3, :3]
+        #TODO: check global velocity
+        joint_global_velocity = joint.child_bodynode.world_linear_velocity(joint.transform_from_child_body_node()[3, :3])
+        joint_global_ang_vel = joint.child_bodynode.world_angular_velocity() - joint.parent_bodynode.world_angular_velocity()
+
+        _Jw = joint_frames[joint_idx][:3, :3]
+        _dJdqw = mm.cross(joint.parent_bodynode.world_angular_velocity(), joint_global_ang_vel)
+
+        for body_idx in range(1, body_num):
+            if joint_idx in ancestors[body_idx]:
+                eff_pos = skel.body(body_idx).to_world()
+                eff_vel = skel.body(body_idx).world_linear_velocity()
+
+                # jacobian
+                offset = eff_pos - joint_global_pos
+                _Jv = -np.dot(mm.getCrossMatrixForm(offset), _Jw)
+
+                if ANGULAR_FIRST:
+                    J[6*body_idx+3:6*body_idx+6, dof_start_index+0:dof_start_index+3] = _Jv
+                    J[6*body_idx+0:6*body_idx+3, dof_start_index+0:dof_start_index+3] = _Jw
+                else:
+                    J[6*body_idx+0:6*body_idx+3, dof_start_index+0:dof_start_index+3] = _Jv
+                    J[6*body_idx+3:6*body_idx+6, dof_start_index+0:dof_start_index+3] = _Jw
+
+                # jacobian derivatives
+                offset_velocity = eff_vel - joint_global_velocity
+                _dJdqv = -mm.cross(offset, _dJdqw) - mm.cross(offset_velocity, joint_global_ang_vel)
+
+                if ANGULAR_FIRST:
+                    dJdq[6*body_idx+3:6*body_idx+6] += _dJdqv
+                    dJdq[6*body_idx+0:6*body_idx+3] += _dJdqw
+                else:
+                    dJdq[6*body_idx+0:6*body_idx+3] += _dJdqv
+                    dJdq[6*body_idx+3:6*body_idx+6] += _dJdqw
+
+    return (J, dJdq)
